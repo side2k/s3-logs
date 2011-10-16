21f71fd9c634c9bc3bfebc37ebf53790fb2976af
outsider <siderelay@gmail.com>
2011-10-16 10:19:12 +0400

    named fields

--- s3_logs.py
+++ s3_logs.py
@@ -1,7 +1,14 @@
+from datetime import datetime
+
 QUOTE_LEFT = '["'
 QUOTE_RIGHT = ']"'
 
+
 def parse_log(lines):
+    """
+        Log format is taken from here:
+        http://s3browser.com/amazon-s3-bucket-logging-server-access-logs.php#amazon_s3_server_access_log_format
+    """
     parsed = []
     for line in lines:
         if line:
@@ -9,21 +16,42 @@ def parse_log(lines):
             tail = line.strip()
             while tail:
                 field, _, tail = tail.partition(' ')
-                #print 'field is: "%s"(%s)' % (field, tail)
                 if field[0] in QUOTE_LEFT:
                     delimiter_index = QUOTE_LEFT.index(field[0])
                     quote_right = QUOTE_RIGHT[delimiter_index]
-                    field_right, _, tail = tail.partition(quote_right)
-                    tail = tail.strip()
-                    fields += ['%s %s' % (field[1:], field_right)]
+                    if (len(field) > 1) and (field[-1] == quote_right):
+                        fields += [field[1:-1]]
+                    else:
+                        field_right, _, tail = tail.partition(quote_right)
+                        tail = tail.strip()
+                        fields += ['%s %s' % (field[1:], field_right)]
                 else:
                     fields += [field]
-            parsed += [fields]
+            log_entry = {
+                'bucket_owner':fields[0],
+                'bucket':fields[1],
+                'timestamp':fields[2],
+                'ip':fields[3],
+                'user':fields[4],
+                'request_id':fields[5],
+                'operation':fields[6],
+                'key':fields[7],
+                'url':fields[8],
+                'http_status':fields[9],
+                'error_code':fields[10],
+                'bytes_sent':fields[11],
+                'object_size':fields[12],
+                'time':fields[13],
+                'time_turnaround':fields[14],
+                'referrer':fields[15],
+                'user_agent':fields[16],
+                'version_id':fields[17],
+            }
+            parsed += [log_entry]
     return parsed
         
 f = open('logs/2011-10-14-00-16-56-B066D716F32D84BA', 'r')
 lines = f.readlines()
 f.close()
 parsed_data = parse_log(lines)        
-for field in parsed_data[0]:
-    print field
+print parsed_data[0]
