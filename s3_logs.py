afb603964ee07b936f73eed4cad4a83190ffadde
outsider <siderelay@gmail.com>
2011-10-16 19:31:38 +0400

    imported some basic routines from munin-s3-size plugin, added functions for listing buckets and getting log list

--- s3_logs.py
+++ s3_logs.py
@@ -1,9 +1,13 @@
 import os
 from os import path
+import subprocess
 from datetime import datetime, timedelta
 
 QUOTE_LEFT = '["'
 QUOTE_RIGHT = ']"'
+
+CACHE_PATH_DEFAULT = '/tmp/munin_s3_logs.cache/'
+CACHE_EXPIRES_DEFAULT = 3600 #seconds
 
 def get_int(int_str):
     return 0 if int_str == '-' else int(int_str)
@@ -71,7 +75,71 @@ def read_log_file(filename):
     lines = f.readlines()
     f.close()
     return parse_log(lines)        
+    
+def parse_ls(lines):
+    result = []
+    for line in lines:
+        if not line.strip():
+            continue
+        fields = [field for field in line.strip().split(' ') if field]
+        result.append({
+            'timestamp':datetime.strptime(
+                '%s %s' % (fields[0], fields[1]),
+                '%Y-%m-%d %H:%M'),
+            'size':int(fields[2]),
+            'path':fields[3]})
+    return result    
+    
+def get_buckets():
+    process = subprocess.Popen(['s3cmd', 'ls'], stdout=subprocess.PIPE)
+    buckets = []
+    for line in process.stdout.readlines():
+        _, _, bucket_name = line.partition('s3://')
+        if bucket_name:
+            buckets += [bucket_name.strip()]
+    return buckets
 
+def get_log_list(bucket_name):
+    process = subprocess.Popen(['s3cmd', 'ls', 's3://%s/logs/' % bucket_name], stdout=subprocess.PIPE)
+    return parse_ls(process.stdout.readlines())
+    
+    
+def get_cached_data(cache_path = CACHE_PATH_DEFAULT):
+    if 's3_cache_expires' in os.environ:
+        try:
+            cache_expires = int(os.environ['s3_cache_expires'])
+        except:
+            cache_expires = CACHE_EXPIRES_DEFAULT
+    else:
+        cache_expires = CACHE_EXPIRES_DEFAULT
+    cache_valid = False
+    if os.path.exists(cache_path):
+        file_stats = os.stat(cache_path)
+        cache_update = datetime.fromtimestamp(file_stats.st_mtime)
+        update_delta = datetime.now() - cache_update
+        cache_valid = \
+            (update_delta.days * 86400 +update_delta.seconds) < cache_expires
+    else:
+        os.mkdir(cache_path)
+        os.chmod(cache_path, stat.S_IRUSR | stat.S_IWUSR)
+        
+    if not cache_valid:
+        s3_list = get_file_list()
+        cache_file = open(cache_path, 'w')
+        cache_file.writelines(s3_list)
+        cache_file.close()
+        os.chmod(cache_path, stat.S_IRUSR | stat.S_IWUSR)
+    else:
+        cache_file = open(cache_path, 'r')
+        s3_list = cache_file.readlines()
+        cache_file.close()
+        
+    return parse_list(s3_list)
+
+def normalize_name(name):
+    normal_first = re.sub(r'^[^A-Za-z_]', r'_', name)
+    return re.sub(r'[^A-Za-z0-9_]', r'_', normal_first)
+    
 operations = {}    
 log_files = os.listdir('logs/')
 for log_filename in log_files:
@@ -86,3 +154,5 @@ for log_filename in log_files:
                 operations[operation] = element['transfer_bytes']
 for operation, traffic in operations.iteritems():
     print operation, ': ', traffic
+
+print get_log_list('asdarh.ru')
